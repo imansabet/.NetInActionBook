b96b0d95f2228aeb3f39d1ed757920bbc0d048b6
iman <dev.imansabet@gmail.com>
2023-12-17 08:50:43 +0330

    3.6 Define Program

--- webApi/Program.cs
+++ webApi/Program.cs
@@ -1,6 +1,10 @@
+//Creates a webApplication Builder using the CreateBuilder Method
 var builder = WebApplication.CreateBuilder(args);
+//Build and Return an instance from WebApplication Builder
 var app = builder.Build();
 
+//Defines an Endpoint for application , return "Hello world" when the path "/" is callled  -- path: reminder of the request URL after domain remove
 app.MapGet("/", () => "Hello World!");
 
+//Runs the Application to start listening for requests and generating responses
 app.Run();
